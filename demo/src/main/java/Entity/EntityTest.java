1f36b74a4ac92b9eb3ff818de6da3aca29ce0988
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-07-30 22:28:14 -0300

    Indexando as anotações criando a classe Entity

---
+++ demo/src/main/java/Entity/EntityTest.java
@@ -0,0 +1,13 @@
+package Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Tb_Test")
+
+public class EntityTest implements Serializable {
+}
