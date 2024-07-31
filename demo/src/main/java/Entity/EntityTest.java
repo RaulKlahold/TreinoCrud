1a1305c43585be0953b22a8dafadbac30c9b9387
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-07-30 22:36:35 -0300

    criação da entidade a prmiera parte

--- demo/src/main/java/Entity/EntityTest.java
+++ demo/src/main/java/Entity/EntityTest.java
@@ -1,11 +1,12 @@
 package Entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Entity
 @Table(name = "Tb_Test")
@@ -13,5 +14,11 @@ import java.io.Serializable;
 public class EntityTest implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
 
 }
