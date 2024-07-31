0ecdb0148bebb5c1c53798ef9498d6af89ad56e7
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-07-30 22:30:20 -0300

    Implementando o seriarizabel

--- demo/src/main/java/Entity/EntityTest.java
+++ demo/src/main/java/Entity/EntityTest.java
@@ -4,10 +4,14 @@ package Entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "Tb_Test")
 
 public class EntityTest implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
 }
