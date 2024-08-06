5aba9c7a98079fa6d3b009fac43476645e24999a
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-08-05 22:40:41 -0300

    iniciando a criação do controller

--- demo/src/main/java/Controllers/ProductController.java
+++ demo/src/main/java/Controllers/ProductController.java
@@ -1,4 +1,15 @@
 package Controllers;
 
+
+import Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
 }
